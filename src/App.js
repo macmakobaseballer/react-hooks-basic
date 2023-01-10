d6a328fa0f5f90972a254addaaa41bad00a4a322
山口諒 <>
2023-01-10 23:18:09 +0900

    initial commit：useEffect learning

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import BasicUseEffect from "./components/BasicUseEffect";
+import TimerContainer from "./components/TimerContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* <BasicUseEffect /> */}
+        <TimerContainer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
